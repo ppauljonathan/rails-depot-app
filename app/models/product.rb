91dc6d743172871a4855b2ed4b697b6dfe0969f9
P Paul Jonathan <ppauljonathan@gmail.com>
2023-03-28 15:14:35 +0530

    task g: check out

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,12 @@
 class Product < ApplicationRecord
   has_many :line_items
-
+  validates :title, presence: true
+  validates :description, length: { minimum: 10 }
+  validates :price, numericality: { greater_than_or_equal_to: 0.01 }
+  validates :image_url, allow_blank: true, format: {
+    with: %r{\.(gif|jpg|png)\z}i,
+    message: 'must be a URL for GIF, JPG or PNG image.'
+  }
   before_destroy :ensure_not_referenced_by_any_line_items
 
   private
