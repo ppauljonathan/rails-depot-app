51b70fac9e0060b0b9ee7aeeb442184883b43c93
P Paul Jonathan <ppauljonathan@gmail.com>
2023-03-28 17:42:11 +0530

    task h: entering additional payment details

---
+++ app/javascript/packs/pay_type.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import PayTypeSelector from '../PayTypeSelector'
+
+let element = document.getElementById('pay-type-component');
+ReactDOM.render(<PayTypeSelector />, element);
