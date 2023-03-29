33ce1fa328d31c92e7b89d1628c5fbd113d0ee80
P Paul Jonathan <ppauljonathan@gmail.com>
2023-03-29 13:13:46 +0530

    task j: loggin in

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,7 +1,8 @@
 class LineItemsController < ApplicationController
   include CurrentCart
+  skip_before_action :authorize, only: [:create]
   before_action :set_cart, only: [:create]
-  before_action :set_line_item, only: %i[ show edit update destroy ]
+  before_action :set_line_item, only: %i[show edit update destroy]
 
   # GET /line_items or /line_items.json
   def index
