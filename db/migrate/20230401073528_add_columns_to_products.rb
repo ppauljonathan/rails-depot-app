24bcd0d447e1c82f3b2bd2d88f4942133d1c3270
P Paul Jonathan <70207656+ppauljonathan@users.noreply.github.com>
2023-04-05 11:42:50 +0530

    Update 20230401073528_add_columns_to_products.rb
    used ```change_table``` instead of ```add_column```

--- db/migrate/20230401073528_add_columns_to_products.rb
+++ db/migrate/20230401073528_add_columns_to_products.rb
@@ -1,7 +1,9 @@
 class AddColumnsToProducts < ActiveRecord::Migration[7.0]
   def change
-    add_column :products, :enabled, :boolean
-    add_column :products, :discount_price, :decimal, precision: 8, scale: 2
-    add_column :products, :permalink, :string
+    change_table :products do |t|
+      t.boolean :enabled
+      t.decimal :discount_price, precision: 8, scale: 2
+      t.string :permalink
+    end
   end
 end
